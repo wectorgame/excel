1a6e63040f3896e813fa384bc015994f2f785667
wectorgame <wectorgame@gmail.com>
2020-12-13 16:25:12 +0300

    add dynamic table

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,61 +1,14 @@
-import {ExcelComponent} from "../../core/ExcelComponent";
+import {ExcelComponent} from "@core/ExcelComponent";
+import {createTable} from "./table.template";
 /**
  *
  */
 export class Table extends ExcelComponent {
   static className = 'excel__table'
   /**
-   * @return {string}
+   * @return {@}
    */
   toHTML() {
-    return `<div class="row">
-    <div class="row-info"></div>
-    <div class="row-data">
-      <div class="column">A</div>
-      <div class="column">B</div>
-      <div class="column">C</div>
-      <div class="column">A</div>
-      <div class="column">B</div>
-      <div class="column">C</div>
-      <div class="column">A</div>
-      <div class="column">B</div>
-      <div class="column">C</div>
-      <div class="column">A</div>
-      <div class="column">B</div>
-      <div class="column">C</div>
-      <div class="column">A</div>
-      <div class="column">B</div>
-      <div class="column">C</div>
-      <div class="column">A</div>
-      <div class="column">B</div>
-      <div class="column">C</div>
-      <div class="column">A</div>
-      <div class="column">B</div>
-      <div class="column">C</div>
-      <div class="column">A</div>
-      <div class="column">B</div>
-      <div class="column">C</div>
-      <div class="column">A</div>
-      <div class="column">B</div>
-      <div class="column">C</div>
-      
-    </div>
-  </div>
-  <div class="row">
-    <div class="row-info">1</div>
-    <div class="row-data">
-      <div class="cell selected" contenteditable="">a1</div>
-      <div class="cell" contenteditable="">b1</div>
-      <div class="cell" contenteditable="">c1</div>
-    </div>
-  </div>
-  <div class="row">
-    <div class="row-info">2</div>
-    <div class="row-data">
-      <div class="cell selected" contenteditable="">a1</div>
-      <div class="cell" contenteditable="">b1</div>
-      <div class="cell" contenteditable="">c1</div>
-    </div>
-  </div>`;
+    return createTable(20);
   }
 }
